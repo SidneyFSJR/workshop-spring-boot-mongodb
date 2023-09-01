f750caf76eebb53cafed87b884bc317566402f30
Sidney Ferreira <sidneyfsjr@gmail.com>
2023-09-01 12:24:46 -0300

    Consulta simples com @Query

--- src/main/java/com/sidneyferreira/workshopmongo/services/PostService.java
+++ src/main/java/com/sidneyferreira/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.sidneyferreira.workshopmongo.services.exception.ObjectNotFoundExcepti
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,4 +21,7 @@ public class PostService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
+    }
 }
